ab3cc5342c04b41bade431318040eea302914789
Umang Parmar <umangjparmar@gmail.com>
2021-05-09 23:11:57 +0530

    add routing logic

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -19,20 +23,43 @@ type Paste struct {
 }
 
 var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("ap-south-1"))
+var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
 
-func createPaste(paste string) {
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+	errorLogger.Println(err.Error())
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Body:       http.StatusText(http.StatusInternalServerError),
+	}, nil
+}
+
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       http.StatusText(status),
+	}, nil
+}
+
+func createPaste(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if req.Headers["content-type"] != "application/json" && req.Headers["Content-Type"] != "application/json" {
+		return clientError(http.StatusNotAcceptable)
+	}
+
+	var paste Paste
+	err := json.Unmarshal([]byte(req.Body), paste)
 	ct := time.Now()
 	item := Paste{
 		Name:      "upaste_" + fmt.Sprint(ct.Unix()),
-		Data:      paste,
+		Data:      paste.Data,
 		CreatedAt: ct,
 	}
 	av, err := dynamodbattribute.MarshalMap(item)
 	if err != nil {
 		log.Fatalf("Got error marshalling new movie item: %s", err)
-		return
+		return serverError(err)
 	}
-	// Create item in table Movies
+
 	tableName := "pastes"
 
 	input := &dynamodb.PutItemInput{
@@ -43,10 +70,21 @@ func createPaste(paste string) {
 	_, err = db.PutItem(input)
 	if err != nil {
 		log.Fatalf("Got error calling PutItem: %s", err)
-		return
+		return serverError(err)
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusCreated,
+		Body:       http.StatusText(http.StatusCreated),
+	}, nil
+}
+
+func router(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "POST":
+		return createPaste(req)
 	}
 }
 
 func main() {
-	lambda.Start(createPaste)
+	lambda.Start(router)
 }
